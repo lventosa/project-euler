bb112e88e6118849450c83b4e56949150c3ae1de
lventosa <laura.ventosa01@estudiant.upf.edu>
2020-12-08 12:59:39 +0100

    to notebook

--- pr5.py
+++
@@ -1,3 +0,0 @@
-# We have to compute the least common multiple of the numbers 1 to 20
-lcm = 2*2*2*2*3*3*5*7*11*13*17*19
-print(lcm)
--- pr6.py
+++
@@ -1,7 +0,0 @@
-ssq = 0
-suma = 0
-for i in range(0, 101):
-  suma += i
-  ssq += i*i
-suma = suma*suma
-print(suma-ssq)
